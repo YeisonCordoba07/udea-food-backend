b46ed55d2813bbf42a42f89ec9a9aa206450c2eb
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2025-03-05 21:06:39 -0500

    first commit

---
+++ src/main/java/com/udeafood/UdeaFoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.udeafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdeaFoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdeaFoodBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udeafood/UdeaFoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udeafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdeaFoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
